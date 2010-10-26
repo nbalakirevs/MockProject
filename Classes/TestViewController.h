c0b6813efafac7561c7be3dfbf21e7f3671fa4ac
Nikita Balakirevs <nb@grandcentrix.com>
2010-10-26 11:01:35 +0200

    added new controller

---
+++ Classes/TestViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestViewController.h
+//  MockProject
+//
+//  Created by Nikita Balakirevs on 10/26/10.
+//  Copyright 2010 Grand Centrix GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TestViewController : UIViewController {
+
+}
+
+@end
